1b12c0f541e7a25c35cbf731f4d3cca36fb99ed6
Perego, Diego <>
2024-11-21 09:14:53 +0100

    fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.mediapipe.genai)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.play.services.mlkit.text.recognition.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
